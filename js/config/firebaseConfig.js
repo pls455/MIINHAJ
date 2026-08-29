cf84c2f0b3aa0ead6689e588f1a2ba5c0b7c6174
pls455 <karamhahhss123@gmail.com>
2026-08-29 19:11:41 +0300

    feat: centralize Firebase client configuration

---
+++ js/config/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = Object.freeze({
+  apiKey: "AIzaSyCSe5Rgsr6_JneRvhtZilHmTfga8LCYZUo",
+  authDomain: "minhaj-1be6f.firebaseapp.com",
+  projectId: "minhaj-1be6f",
+  storageBucket: "minhaj-1be6f.firebasestorage.app",
+  messagingSenderId: "780231348130",
+  appId: "1:780231348130:web:cafdef92ee081e12b02cfb",
+});
